b8cf4ce2054b009d8e2da66eb38a9688d39d96c4
Débora Lucia Koppe Soares <deboraluciak@hotmail.com>
2023-06-01 13:51:18 -0300

    estudo componentes

---
+++ src/components/InfosUsuario.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function InfosUsuario() {
+
+  return (
+    <div>
+        <h4>Usuario</h4>
+        <img src="https://crn10.org.br/wp-content/uploads/2021/09/perfil-300x300-4.jpg" alt="" />
+    </div>
+  );
+}
+
+export default InfosUsuario;
